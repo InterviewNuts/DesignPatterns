71f2a874704610bdf1df10e614f8bf75cb4b2226
InterviewNuts <96710519+InterviewNuts@users.noreply.github.com>
2022-01-22 23:31:04 +0530

    Builder patter added

---
+++ BuilderPattern/test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class HttpClient
+{
+    string method;
+public:
+    class nnn
+    {
+        string method;
+        nnn* method_(string method)
+        {
+            this->method=method;
+            return this;
+        }
+    };
+};
+int main()
+{
+
+}
