0e4064f678be94b64304130dac525a91d2e9290a
InterviewNuts <96710519+InterviewNuts@users.noreply.github.com>
2022-02-12 15:26:00 +0530

    Behavioral patterns added

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+#include <stdio.h> 
+
+
+#include<stdio.h> 
+#define begin(m,a,i,n) m##a##i##n 
+#define start begin(m,a,i,n) 
+void start() {
+    printf("OmkarPathak");
+}
